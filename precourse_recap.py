e62ac91994e5237a51a5eefa90c76cdc0c78f9cb
burgic <ceburgin@gmail.com>
2022-02-21 19:47:20 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,15 @@
+from asyncio.base_events import _ProtocolFactory
+
+word_one = "hello"
+# print(word_one)
+
+NumberOne = 1
+
+NumberTwo = 2
+
+Maths = NumberOne + NumberTwo
+
+print(Maths)
+print(word_one)
+
+fruit = potato
